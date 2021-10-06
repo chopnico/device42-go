0511961f56f3718e5daaebe1a8400c4e01f75df9
Wes Key <2540699+chopnico@users.noreply.github.com>
2021-10-06 10:19:42 -0400

    added GetSubnetsByParentSubnetIDWithVRFGroupID

--- ipam_subnet.go
+++ ipam_subnet.go
@@ -294,6 +294,31 @@ func (api *API) GetSubnetsByParentSubnetID(i int) (*[]Subnet, error) {
 	return &subnets.List, nil
 }
 
+// GetSubnetsByParentSubnetIDWithVRFGroupID will return a subnet by a given name
+func (api *API) GetSubnetsByParentSubnetIDWithVRFGroupID(p, v int) (*[]Subnet, error) {
+	b, err := api.Do(
+		"GET",
+		"/subnets/"+"?parent_subnet_id="+url.QueryEscape(strconv.Itoa(p)) +"&vrf_group_id="+strconv.Itoa(v),
+		nil)
+	if err != nil {
+		return nil, err
+	}
+
+	subnets := Subnets{}
+
+	err = json.Unmarshal(b, &subnets)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(subnets.List) == 0 {
+		return nil, errors.New("unable to find subnet with parent subnet id " + strconv.Itoa(i))
+	}
+
+	return &subnets.List, nil
+}
+
+
 // GetSubnetByID will return a subnet by an ID
 func (api *API) GetSubnetByID(id int) (*Subnet, error) {
 	b, err := api.Do(
